ae573cd2264a01072e2763bb5acfa68cae7c5bcf
suminjung09 <suminjung@boomco.org>
2022-01-05 14:59:15 +0900

    feat: complete instantiate contract

--- test-contract-scripts/contract.py
+++ test-contract-scripts/contract.py
@@ -1,7 +1,7 @@
 from terra_sdk.client.localterra import LocalTerra
-from terra_sdk.util.contract import read_file_as_b64, get_code_id
+from terra_sdk.util.contract import read_file_as_b64, get_code_id, get_contract_address
 from terra_sdk.core.auth import StdFee
-from terra_sdk.core.wasm import MsgStoreCode
+from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract
 
 from terra_sdk.key.mnemonic import MnemonicKey
 from terra_sdk.client.lcd import LCDClient
@@ -21,11 +21,28 @@ def store_contract(contract_name: str) -> str:
     )
 
     tx = deployer.create_and_sign_tx(
-        msgs = [store_code], fee=StdFee(4000000, "10000000uluna")
+        msgs = [store_code], fee=StdFee(40000000, "10000000uluna")
     )
 
     result = terra.tx.broadcast(tx)
+    print(result)
     code_id = get_code_id(result)
+
     return code_id
 
-print(store_contract("cw20_base"))
+def instantiate_contract(code_id: str, init_msg) -> str:
+    instantiate = MsgInstantiateContract(
+        admin=deployer.key.acc_address, sender=deployer.key.acc_address , code_id=code_id, init_msg=init_msg
+    )
+    tx = deployer.create_and_sign_tx(
+        msgs=[instantiate], fee=StdFee(40000000, "10000000uluna")
+    )
+    result = terra.tx.broadcast(tx)
+    # contract_address = get_contract_address(result)
+    print(result)
+    # print(contract_address)
+    # return contract_address
+
+code_id = store_contract("cw20_base")
+contract_address = instantiate_contract(code_id, {"name":"real_token","symbol":"SYMBOL","decimals": 3,"initial_balances":[{"address":"terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v","amount":"10000"},{"address":"terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp","amount":"10000"}]})
+# print(terra.wasm.contract_query(contract_address, {"balance":{"address": "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v"}}))
