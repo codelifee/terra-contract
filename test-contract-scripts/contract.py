8d478669b012e76fead8727cedf569d89d7e642c
suminjung09 <suminjung@boomco.org>
2022-01-05 17:03:39 +0900

    feat: complete token pair func

--- test-contract-scripts/contract.py
+++ test-contract-scripts/contract.py
@@ -1,7 +1,9 @@
+from dis import code_info
+
 from terra_sdk.client.localterra import LocalTerra
 from terra_sdk.util.contract import read_file_as_b64, get_code_id, get_contract_address
 from terra_sdk.core.auth import StdFee
-from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract
+from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract, MsgExecuteContract
 
 from terra_sdk.key.mnemonic import MnemonicKey
 from terra_sdk.client.lcd import LCDClient
@@ -25,6 +27,7 @@ def store_contract(contract_name: str, sequence) -> str:
     )
 
     result = terra.tx.broadcast(tx)
+
     code_id = get_code_id(result)
 
     return code_id
@@ -44,9 +47,74 @@ def instantiate_contract(code_id: str, init_msg, sequence) -> str:
 
     return contract_address
 
+
+def execute_contract(
+        sender, contract_addr: str, execute_msg, sequence, coins=None
+):
+    execuete = MsgExecuteContract(
+        sender=sender.key.acc_address,
+        contract=contract_addr,
+        execute_msg=execute_msg,
+        coins=coins
+    )
+
+    print(f"ddd {sequence}")
+
+    tx = sender.create_and_sign_tx(
+        msgs=[execuete], fee=StdFee(400000000, "10000000uluna"), sequence=sequence
+    )
+
+    print(f"ddd {tx}")
+
+    result = terra.tx.broadcast(tx)
+
+
+    return result
+
 # we need to increase the sequence cuz it runs too fast. the sequence overlaps
 sequence = terra.auth.account_info(deployer.key.acc_address).sequence
 
-code_id = store_contract("cw20_base", sequence)
-contract_address =instantiate_contract(code_id, {"name":"real_token","symbol":"SYMBOL","decimals": 3,"initial_balances":[{"address":"terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v","amount":"10000"},{"address":"terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp","amount":"10000"}]}, sequence+1)
-print(terra.wasm.contract_query(contract_address, {"balance":{"address": "terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp"}}))
+token_code_id = store_contract("cw20_base", sequence)
+# contract_address =instantiate_contract(token_code_id, {"name":"aurora","symbol":"SYMBOL","decimals": 3,"initial_balances":[{"address":"terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v","amount":"10000"},{"address":"terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp","amount":"10000"}]}, sequence+1)
+# print(terra.wasm.contract_query(contract_address, {"balance":{"address": "terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp"}}))
+pair_code_id = store_contract("terraswap_pair", sequence+1)
+factory_code_id = store_contract("terraswap_factory", sequence+2)
+
+# create normal ts factory
+factory = instantiate_contract(
+    factory_code_id,
+    {"token_code_id": int(token_code_id), "pair_code_id": int(pair_code_id)},
+    sequence+3
+)
+
+# create aurora token
+aurora_token = instantiate_contract(
+    token_code_id,
+    {
+        "name": "Aurora Token",
+        "symbol": "NORM",
+        "decimals": 6,
+        "initial_balances": [
+            {"address": deployer.key.acc_address, "amount": "10000000"}
+        ]
+    },
+    sequence+4
+)
+
+# create aurora-LUNA pair
+result = execute_contract(
+    deployer,
+    factory,
+    {
+        "create_pair": {
+            "asset_infos": [
+                {"token": {"contract_addr": aurora_token}},
+                {"native_token": {"denom": "uluna"}},
+            ]
+        }
+    },
+    sequence+5
+)
+
+token_pair = result.logs[0].events_by_type["from_contract"]["pair_contract_addr"][0]
+print(f"token pair contract: {token_pair}")
