1e1d3f1cf67d486f2e7fa5be699e3ce76d85c6b8
suminjung09 <suminjung@boomco.org>
2022-01-17 19:20:57 +0900

    FEAT: complete sale-transaction

--- test-contract-scripts/custom-sale-contract.py
+++ test-contract-scripts/custom-sale-contract.py
@@ -29,14 +29,108 @@ def store_contract(contract_name: str, sequence) -> str:
 
     result = terra.tx.broadcast(tx)
 
-    print(result)
-
     code_id = get_code_id(result)
 
     return code_id
 
+def instantiate_contract(code_id: str, init_msg, sequence) -> str:
+    instantiate = MsgInstantiateContract(
+        admin=deployer.key.acc_address, sender=deployer.key.acc_address , code_id=code_id, init_msg=init_msg
+    )
+    tx = deployer.create_and_sign_tx(
+        msgs=[instantiate], fee=StdFee(400000000, "10000000uluna"), sequence=sequence
+    )
+
+    result = terra.tx.broadcast(tx)
+
+    contract_address = get_contract_address(result)
+
+    return contract_address
+
+
+def execute_contract(
+        sender, contract_addr: str, execute_msg, sequence, coins=None
+):
+    execuete = MsgExecuteContract(
+        sender=sender.key.acc_address,
+        contract=contract_addr,
+        execute_msg=execute_msg,
+        coins=coins
+    )
+
+    tx = sender.create_and_sign_tx(
+        msgs=[execuete], fee=StdFee(400000000, "10000000uluna"), sequence=sequence
+    )
+
+    result = terra.tx.broadcast(tx)
+
+    print(result)
+
+    return result
+
 
 sequence = terra.auth.account_info(deployer.key.acc_address).sequence
 
-token_code_id = store_contract("cosmwasm_16", sequence)
+token_code_id = store_contract("astroport_lbp_token", sequence)
+sale_code_id = store_contract("cosmwasm_16", sequence+1)
 
+# create aurora token
+boomco_token = instantiate_contract(
+    token_code_id,
+    {
+        "name": "Boomco Token",
+        "symbol": "Boomco",
+        "decimals": 6,
+        "initial_balances": [
+            {"address": deployer.key.acc_address, "amount": remove_decimal_point("100.000000")}
+        ]
+    },
+    sequence+2
+)
+
+sale_contract = instantiate_contract(
+    sale_code_id,
+    {
+        "cw20_address": boomco_token,
+        "denom": "uusd",
+        "price": "1"
+    },
+    sequence+3
+)
+
+# Deposit cw20 Tokens
+execute_contract(
+    deployer,
+    boomco_token,
+    {"send":
+         {"amount":"100",
+          "contract":sale_contract,
+          "msg":""}
+     },
+    sequence+4
+)
+
+# Set Price
+execute_contract(
+    deployer,
+    sale_contract,
+    {"set_price":
+         {"price":"1",
+          "denom":"uusd"}
+     },
+    sequence+5
+)
+
+# Buy cw20 coin
+execute_contract(
+    deployer,
+    sale_contract,
+    {
+        "buy":{
+            "denom": "uusd",
+            "price": "1",
+        }
+    },
+    sequence+6,
+    "2uusd"
+)
